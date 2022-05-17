ae3b46bc9a70ea8d9d952b6dea181c48a19dd9ea
Maryam Sadeghi <maryammaryamsadeghi6@gmail.com>
2022-05-17 15:52:09 +0430

    add some comments

--- src/utils/socketAuth.js
+++ src/utils/socketAuth.js
@@ -1,17 +1,24 @@
+/** @format */
+
+//Once the user is logged in,
+// each subsequent request will include the JWT,
+//allowing the user to access routes, services,
+//and resources that are permitted with that token
+
 const jwt = require("jsonwebtoken");
 const { PASSPORT_SECRET_KEY } = process.env;
 module.exports.socketAuth = function (socket, next) {
-  if (socket.handshake.query && socket.handshake.query.token) {
-    jwt.verify(
-      socket.handshake.query.token,
-      PASSPORT_SECRET_KEY,
-      function (err, decoded) {
-        if (err) return next(new Error("Authentication error"));
-        socket.user = decoded;
-        next();
-      }
-    );
-  } else {
-    next(new Error("Authentication error"));
-  }
+	if (socket.handshake.query && socket.handshake.query.token) {
+		jwt.verify(
+			socket.handshake.query.token,
+			PASSPORT_SECRET_KEY,
+			function (err, decoded) {
+				if (err) return next(new Error("Authentication error"));
+				socket.user = decoded;
+				next();
+			}
+		);
+	} else {
+		next(new Error("Authentication error"));
+	}
 };
