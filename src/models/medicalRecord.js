9fb321d6685e8fed55785572933bb0f1f4b7943d
Maryam Sadeghi <maryammaryamsadeghi6@gmail.com>
2022-05-17 21:08:51 +0430

    the model of MedicalSchema has changed

--- src/models/medicalRecord.js
+++ src/models/medicalRecord.js
@@ -6,38 +6,23 @@ const MedicalSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	patientId: { type: mongoose.Schema.Types.ObjectId, required: true },
 	doctorId: { type: mongoose.Schema.Types.ObjectId, required: true },
-	symptoms: [{ type: String, required: true }],
-	diagnosis: { type: String, required: true },
 
-	description: {
-		type: String,
-		minlength: 1,
-		maxlength: 1000,
-		trim: true,
-	},
-	medication: [
+	items: [
 		{
-			_id: {
-				type: mongoose.Schema.Types.ObjectId,
-				index: true,
-				auto: true,
-			},
 			name: {
 				type: String,
 				minlength: 1,
 				maxlength: 255,
-				trim: true,
 			},
-			dosage: {
+			count: {
 				type: Number,
 				min: 1,
 				max: 10000,
 			},
-			manufacturer: {
+			description: {
 				type: String,
 				minlength: 1,
-				maxlength: 255,
-				trim: true,
+				maxlength: 1000,
 			},
 		},
 	],
